305c2c76738130c29579b52e60b41cd785f8b4a8
Aryan Agarwal <aryankush025@gmail.com>
2021-04-08 15:11:58 +0530

    Build files removed

--- user-server/build/utils/envConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PORT = process.env.PORT;
-exports.default = {
-    port: Number(PORT),
-};
-//# sourceMappingURL=envConfig.js.map

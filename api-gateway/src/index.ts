d7ea0ed78d9fcd0836507a1603bf9f91216becd6
Aryan Agarwal <aryankush025@gmail.com>
2021-04-10 05:19:36 +0530

    GraphQL gateway added

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,16 +1,50 @@
 import express from 'express';
+import axios from 'axios';
 import { ApolloServer, gql } from 'apollo-server-express';
 import envConfigs from './utils/envConfig';
 
+const baseUrl = 'http://localhost:5000/';
+const userBaseUrl = baseUrl + 'user';
+
 const typeDefs = gql`
+  type User {
+    id: ID
+    name: String
+    email: String
+    hashedPassword: String
+    createdAt: String
+    updatedAt: String
+    deletedAt: String
+  }
+
   type Query {
-    hello: String
+    getUser(email: String): User
+  }
+
+  type Mutation {
+    createUser(name: String, email: String): User
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello Aryan!',
+    getUser: async (parent, args) => {
+      console.log(args.email);
+
+      const response = await axios.get(userBaseUrl + '/' + args.email);
+
+      return response.data;
+    },
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const response = await axios.post(userBaseUrl, {
+        name: args.name,
+        email: args.email,
+      });
+
+      return response.data;
+    },
   },
 };
 

3b5880d117826bb58a40fb67f374d788d1ebeef8
Aryan Agarwal <aryankush025@gmail.com>
2021-04-14 01:56:28 +0530

    Hooks example added

--- user-server/src/routes/user/index.ts
+++ user-server/src/routes/user/index.ts
@@ -2,13 +2,27 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import userController from '../../controller/userController';
 import { FastifyInstanceExtended } from '../../server';
 import * as schemas from './schema';
+import * as hooks from './hooks';
 
 export const registerUserRoutes = (server: FastifyInstanceExtended) => {
-  server.post('/user', { schema: schemas.registerRoute }, (request: FastifyRequest, reply: FastifyReply) => {
-    return userController.register(server.db, request, reply);
-  });
+  server.post(
+    '/user',
+    {
+      schema: schemas.registerRoute,
+      preValidation: hooks.registerRoutePreValidation,
+    },
+    (request: FastifyRequest, reply: FastifyReply) => {
+      return userController.register(server.db, request, reply);
+    },
+  );
 
-  server.get('/user/:email', { schema: schemas.meRoute }, (request: FastifyRequest, reply: FastifyReply) => {
-    return userController.me(server.db, request, reply);
-  });
+  server.get(
+    '/user/:email',
+    {
+      schema: schemas.meRoute,
+    },
+    (request: FastifyRequest, reply: FastifyReply) => {
+      return userController.me(server.db, request, reply);
+    },
+  );
 };
--- user-server/src/utils/helpers.ts
+++ user-server/src/utils/helpers.ts
@@ -5,3 +5,10 @@ export const isNilOrEmpty = R.anyPass([R.isNil, R.isEmpty]);
 export const isPresent = R.complement(isNilOrEmpty);
 export const removeNullOrEmptyKeys = (object: any) =>
   R.filter((value: any) => !(isNilOrEmpty(value) || value === -1), object);
+export const wait = (time: number) => {
+  return new Promise((resolve) => {
+    setInterval(() => {
+      resolve({});
+    }, time);
+  });
+};

b765e720921305da8645fd7d0ca36576efa8dabb
Aryan Agarwal <aryankush025@gmail.com>
2021-04-13 12:35:51 +0530

    USer controller extra code removed

--- user-server/src/controller/UserController.ts
+++ user-server/src/controller/UserController.ts
@@ -4,44 +4,32 @@ import { isNilOrEmpty } from '../utils/helpers';
 
 const userController = () => {
   const register = async (server: any, request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const userRepository = server.db.userRepository;
+    const userRepository = server.db.userRepository;
 
-      const name = request.body['name'];
-      const email = request.body['email'];
+    const name = request.body['name'];
+    const email = request.body['email'];
 
-      if (isNilOrEmpty(email) || isNilOrEmpty(name)) {
-        throw ArgumentsDoesNotExistError();
-      }
+    const user = await userRepository.createUser(name, email);
 
-      const user = await userRepository.createUser(name, email);
-
-      return user;
-    } catch (error) {
-      return error;
-    }
+    return user;
   };
 
   const me = async (server: any, request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const userRepository = server.db.userRepository;
+    const userRepository = server.db.userRepository;
 
-      const email = request.params['email'];
+    const email = request.params['email'];
 
-      if (isNilOrEmpty(email)) {
-        throw ArgumentsDoesNotExistError();
-      }
-
-      const user = await userRepository.getUser({ where: { email } });
-
-      if (isNilOrEmpty(user)) {
-        throw UserDoesNotExistError();
-      }
-
-      return user;
-    } catch (error) {
-      return error;
+    if (isNilOrEmpty(email)) {
+      throw ArgumentsDoesNotExistError();
     }
+
+    const user = await userRepository.getUser({ where: { email } });
+
+    if (isNilOrEmpty(user)) {
+      throw UserDoesNotExistError();
+    }
+
+    return user;
   };
 
   return {

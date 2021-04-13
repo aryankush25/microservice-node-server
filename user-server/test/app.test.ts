e6a2670f2d3a0ae83c07a12597c99b4e002624ec
Aryan Agarwal <aryankush025@gmail.com>
2021-04-13 17:27:51 +0530

    tests added using tap

--- user-server/test/app.test.ts
+++ user-server/test/app.test.ts
@@ -1,16 +1,16 @@
-import createServer from '../src/server'
-import {test} from 'tap'
+import createServer from '../src/server';
+import { test } from 'tap';
 
 test('Test /ping', async (t) => {
-  const app = createServer()
+  const app = createServer();
 
   const response = await app.inject({
     method: 'GET',
-    url: '/ping'
-  })
+    url: '/ping',
+  });
 
   // console.log('status code: ', response.statusCode)
   // console.log('body: ', response.body)
 
-  t.equal(response.statusCode, 200, 'returns a status code of 200')
-})
+  t.equal(response.statusCode, 200, 'returns a status code of 200');
+});
